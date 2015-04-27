da2438726f78a699fc01bdbd977a6691b987a999
ArifC <cerit_arif@gmx.de>
2015-04-27 22:49:47 +0200

    fd

--- musclepedia/app/controllers/Application.java
+++ musclepedia/app/controllers/Application.java
@@ -7,8 +7,7 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result showSomething(){
+        return ok(login.render());
     }
-
 }
