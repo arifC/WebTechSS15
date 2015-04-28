3f7df321c7b4635ca97e2e6efa4acfdd38ede4e2
ArifC <cerit_arif@gmx.de>
2015-04-28 10:29:05 +0200

    Änderungen an diversen Dingen

--- musclepedia/app/controllers/Application.java
+++ musclepedia/app/controllers/Application.java
@@ -7,7 +7,8 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result showSomething(){
+    public static Result login(){
+
         return ok(login.render());
     }
 }
