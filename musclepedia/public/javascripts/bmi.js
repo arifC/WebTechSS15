eb24be7587be88dd1e80ddd9285fcb05d67f0b4a
ArifC <cerit_arif@gmx.de>
2015-04-30 10:29:55 +0200

    Bilder etc

--- musclepedia/public/javascripts/bmi.js
+++ musclepedia/public/javascripts/bmi.js
@@ -1,28 +1,29 @@
 function bmi(){
-    var gewicht= document.getElementById("gewicht");
-    var groeße= document.getElementById("groeße");
-    if(document.getElementById("myCheck").checked)
-    var geschlecht= document.getElementById("gesch").options[document.getElementById("gesch").selectedIndex].text)
-    if(groeße >2.5){
-        alert("Falsche Eingabe oder du bist zu Groß")
+    var gewicht= document.getElementById("gewicht").value;
+    var groeße= document.getElementById("groeße").value;
+   // var geschlecht= document.getElementById("gesch").options[document.getElementById("gesch").selectedIndex].text;
+    var geschlecht = "männlich";
+    alert(gewicht);
+    if(groeße > 2.5 || gewicht < 30 || gewicht > 300){
+        alert("Falsche Eingabe")
     }
     var bmi = gewicht/(groeße*groeße);
     if(geschlecht ="männlich"){
         switch(bmi){
             case bmi>20:
-                alert("ACHTUNG UNTERGEWICHT!");
+                alert("Untergewicht");
                 break;
             case bmi<=25&&bmi>20:
-                alert("Normalgewicht sollange Sie unter 25 Jahre alt sind");
+                alert("Normalgewicht");
                 break;
             case bmi<=30&&bmi>25:
-                alert("Übergewicht ausser Sie sind über 35 Jahre alt");
+                alert("Übergewicht");
                 break;
             case bmi<=40&&bmi>31:
-                alert("ACHTUNG Adipositas(Fettleibigkeit");
+                alert("Adipositas");
                     break;
             case bmi>40:
-                alert("SUCHEN SIE IHREN ARZT ODER BESTATTER AUF SIE HABEN STARKE FETTLEIBIGKEIT!")
+                alert("Hochgradige Adipositas");
                 break;
             default:
                 alert("Ihr BMI ist keine Zahl...");
@@ -31,19 +32,19 @@ function bmi(){
     }else{
         switch(bmi){
             case bmi>19:
-                alert("ACHTUNG UNTERGEWICHT!");
+                alert("Untergewicht");
                 break;
             case bmi<=24&&bmi>19:
-                alert("Normalgewicht sollange Sie unter 25 Jahre alt sind");
+                alert("Normalgewicht");
                 break;
             case bmi<=30&&bmi>24:
-                alert("Übergewicht ausser Sie sind über 35 Jahre alt");
+                alert("Übergewicht");
                 break;
             case bmi<=40&&bmi>31:
-                alert("ACHTUNG Adipositas(Fettleibigkeit");
+                alert("Adipositas");
                 break;
             case bmi>40:
-                alert("SUCHEN SIE IHREN ARZT ODER BESTATTER AUF SIE HABEN STARKE FETTLEIBIGKEIT!")
+                alert("Hochgradige Adipositas");
                 break;
             default:
                 alert("Ihr BMI ist keine Zahl...");
@@ -51,5 +52,5 @@ function bmi(){
         }
 
     }
-    $("#bmierg").append(bmi.innerHTML)
+    $("#bmierg").html(bmi);
 }
