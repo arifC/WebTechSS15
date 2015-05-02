06ed96c52a54dbc56bba0e6b8aba44fb69682dee
Felix <fritzsche.fe@gmail.com>
2015-05-02 20:47:12 +0200

    beine bauch arme zu übungen hinzugefügt

--- musclepedia/app/controllers/Application.java
+++ musclepedia/app/controllers/Application.java
@@ -7,27 +7,30 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result login(){
-
-        return ok(login.render());
+    public static Result login(){return ok(login.render());
     }
 
-
-    public static Result start() {
-        return ok(home.render());
+    public static Result start() {return ok(home.render());
     }
 
-    public static Result help() {
-        return ok(help.render());
+    public static Result help() {return ok(help.render());
     }
 
-    public static Result oberkoerper() {
-        return ok(oberkoerper.render());
+    public static Result oberkoerper() {return ok(oberkoerper.render());
     }
 
     public static Result studios(){return ok(studios.render());}
 
     public static Result knStudio(){return ok(knStudios.render());}
+
+    public static Result arme() {return ok(arme.render());
+    }
+
+    public static Result beine() {return ok(beine.render());
+    }
+
+    public static Result bauch() {return ok(bauch.render());
+    }
 
 
 }
